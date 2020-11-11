36ec39d716f6013819b3c7d9b2f3590a9acc03e2
JJeferson <48735241+JJeferson@users.noreply.github.com>
2020-11-11 14:49:45 -0300

    tive de pausar os trabalhos agr
    Inicio dos trabalhos para configurar a pagina de perfil

--- app/src/main/java/com/a/modelo_eventos_kotlin/fragments/perfil_Fragment.kt
+++ app/src/main/java/com/a/modelo_eventos_kotlin/fragments/perfil_Fragment.kt
@@ -5,32 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.a.modelo_eventos_kotlin.Adapter.adapter
+import com.a.modelo_eventos_kotlin.ConsumoAPI.listaRecuperaEventos
 import com.a.modelo_eventos_kotlin.R
+import com.a.modelo_eventos_kotlin.modelos.modeloListaEventos
 import kotlinx.android.synthetic.main.perfil_fragment.*
 
 
-/**
- *
- * // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
- * A simple [Fragment] subclass.
- * Use the [perfil_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class perfil_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-   // private var param1: String? = null
-   // private var param2: String? = null
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+
+    }
+
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-        //    param1 = it.getString(ARG_PARAM1)
-         //   param2 = it.getString(ARG_PARAM2)
-            t2.setText("recurso não habilitado ainda Fragmento perfil")
 
 
 
@@ -41,28 +42,7 @@ class perfil_Fragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.perfil_fragment, container, false)
     }
-/*
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment perfil_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            perfil_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-    */
 }
